e2641c41c2d6e0a3b658c8c1ff1b15994530cd93
Jose Yanez <j.leo94@gmail.com>
2021-10-19 11:29:25 -0300

    creacion de navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+
+
+const NavBar =() => {
+    return (
+            <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Sabor Waira</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Productos" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Postres</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Salados</NavDropdown.Item>
+                    
+                    
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+            )           
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// La función render tomar dos parametros (a=elemento que queres mostrar en pantalla y b=el lugar donde lo quiero mostrar)
+ReactDOM.render(<App/> , document.getElementById ("root"))
